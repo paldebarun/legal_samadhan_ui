a1c2b86170d356f61ea84e20881d328d29ba37fb
Debarun <paldebarun27@gmail.com>
2025-12-30 23:50:41 +0530

    did some minor changes

--- components/TeamDetail.tsx
+++ components/TeamDetail.tsx
@@ -63,7 +63,7 @@ const TeamMemberPage: React.FC = () => {
 
   if (loading)
     return (
-      <div className="col-span-full flex justify-center items-center py-20">
+      <div className="col-span-full flex justify-center items-center py-20 h-screen">
         <div className="w-12 h-12 border-4 border-purple-950 border-t-transparent rounded-full animate-spin"></div>
       </div>
     );
